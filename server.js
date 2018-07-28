1e846e5d13ca0f71ece10c7acab963f4dac93bc1
Viyaan <viyaan.86@gmail.com>
2018-07-29 02:30:59 +0530

    static file path changed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/contactmanagementsystem'));
+app.use(express.static(__dirname + '/dist'));
 
 
 app.listen(process.env.PORT || 6060);
