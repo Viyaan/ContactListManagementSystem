53fda13cfbb21d02ab444671fb17d61aa0eda3b8
Viyaan <viyaan.86@gmail.com>
2018-07-29 12:10:17 +0530

    update import statement

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {AuthService} from './auth.service';
-import {User} from '../login/user';
+import {User} from './user';
 import * as jwt_decode from "jwt-decode"; 
 
 
