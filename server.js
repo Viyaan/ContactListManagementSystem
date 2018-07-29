ca01f46aa00967a445f8cdb660ef88cf23273034
Viyaan <viyaan.86@gmail.com>
2018-07-29 12:02:26 +0530

    deplpy heroku

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-//Install express server
 const express = require('express');
 const app = express();
+const path = require('path');
 
-// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
+app.listen(process.env.PORT || 8080);
 
-app.listen(process.env.PORT || 6060);
+//Path Location Strategy
+
+app.get('/*', function(req, res) {
+res.sendFile(path.join(__dirname + '/dist/index.html'));
+})
