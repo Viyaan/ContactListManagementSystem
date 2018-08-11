0c451378be6c85a6dc4f24467bc181145003fd76
Viyaan <viyaan.86@gmail.com>
2018-08-12 00:31:05 +0530

    adding scripts to deploy

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
+//Install express server
 const express = require('express');
-const http = require('http');
-const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname + '/dist')));
+const app = express();
 
-const port = process.env.PORT || '3001';
-app.set('port',port);
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/contact-list-mmanagement'));
 
-//Path Location Strategy
+app.get('/*', function(req, res) {
 
-app.get('*', function(req, res) {
-res.sendFile(path.join(__dirname + 'dist/index.html'));
-})
+    res.sendFile(path.join(__dirname + '/dist/contact-list-mmanagement/index.html'));
+});
 
-const server =http.createServer(app);
-server.listen(port, () => console.log('Running'));
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
